05dbe324733665808493165b07bcaf52e8600bea
Otmane Aboulghit <oaboulgh@e1r1p4.1337.ma>
2024-04-25 08:11:12 +0100

    edited toDoList

--- toDolist/main.js
+++ toDolist/main.js
@@ -4,15 +4,33 @@ let inputField = document.getElementById('inputField');
 
 
 addToDoButton.addEventListener('click', function(){
+	var spanElement = document.createElement('span');
 	var paragraph = document.createElement('p');
-	toDoContainer.appendChild(paragraph);
+	var button = document.createElement('span');
+
+	spanElement.appendChild(paragraph);
+	toDoContainer.appendChild(spanElement);
+	spanElement.appendChild(button);
+
 	paragraph.innerText = inputField.value;
+	button.innerText = " X ";
 	inputField.value = "";
+
+
+
+
+
+
+
+
+
 	paragraph.addEventListener('click', function(){
 		paragraph.style.textDecoration = "line-through";
 	})
-	paragraph.addEventListener('dblclick', function(){
-		toDoContainer.removeChild(paragraph);
+	button.addEventListener('click', function(){
+		spanElement.removeChild(paragraph);
+		spanElement.removeChild(button);
+		toDoContainer.removeChild(spanElement);
 	})
 	
 })
