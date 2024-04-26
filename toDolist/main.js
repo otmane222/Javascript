805059a798c8925895a69bae9734f9e5ea33e482
Otmane Aboulghit <oaboulgh@e1r1p4.1337.ma>
2024-04-26 12:08:46 +0100

    weather card, js on progress

--- toDolist/main.js
+++ toDolist/main.js
@@ -2,6 +2,7 @@ let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
+loadTasks();
 
 addToDoButton.addEventListener('click', function(){
 	var spanElement = document.createElement('span');
@@ -17,13 +18,6 @@ addToDoButton.addEventListener('click', function(){
 	inputField.value = "";
 
 
-
-
-
-
-
-
-
 	paragraph.addEventListener('click', function(){
 		paragraph.style.textDecoration = "line-through";
 	})
@@ -32,5 +26,41 @@ addToDoButton.addEventListener('click', function(){
 		spanElement.removeChild(button);
 		toDoContainer.removeChild(spanElement);
 	})
+	saveTasks();
 	
 })
+
+function saveTasks(){
+	let tasks = [];
+	tasklist = document.querySelectorAll('p').forEach(function(item){
+		tasks.push(item.textContent.trim());
+	});
+
+	localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks() {
+	const tasks = JSON.parse(localStorage.getItem('tasks'));
+	tasks.forEach(function(item){
+		var spanElement = document.createElement('span');
+		var paragraph = document.createElement('p');
+		var button = document.createElement('span');
+
+		spanElement.appendChild(paragraph);
+		toDoContainer.appendChild(spanElement);
+		spanElement.appendChild(button);
+
+		paragraph.innerText = item;
+		button.innerText = " X ";
+
+		paragraph.addEventListener('click', function(){
+			paragraph.style.textDecoration = "line-through";
+		})
+		button.addEventListener('click', function(){
+			spanElement.removeChild(paragraph);
+			spanElement.removeChild(button);
+			toDoContainer.removeChild(spanElement);
+			saveTasks();
+		})
+	})
+}
